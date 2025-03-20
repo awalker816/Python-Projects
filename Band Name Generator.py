dc7435f41163765d25d0ebfd05f27dd41206650d
April Walker <awal706@wgu.edu>
2025-03-20 16:34:01 +0000

    Band Name Generator project added

---
+++ Band Name Generator.py
@@ -0,0 +1,6 @@
+print("The Ultimate Band Name Generator!\n")
+
+name1 = input("What's your favorite color?\n")
+name2 = input("What's the scariest weather or animal?\n")
+
+print("Your ultimate band name could be: " + name1 + " " + name2 + "!")
